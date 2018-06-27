d44c0a44609b0b28c705f04b520e5d6cd284f192
Harry Chan <harry.chan@hkfdt.com>
2018-06-28 00:04:38 +0800

    finished suggestion

--- routes/manga.js
+++ routes/manga.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const {getChapterLinks, getImgLinks} = require('../mangaUtils');
+const {getChapterLinks, getImgLinks, getSuggestions} = require('../utils/manga-utils');
 
 /* GET users listing. */
-router.get('/:mangaName', function(req, res, next) {
+router.get('/:mangaName', (req, res, next) => {
   const mangaName = req.params.mangaName;
   const mangaURL = 'https://www.manhuaren.com/' + mangaName
   getChapterLinks(mangaURL).then((r) => res.send(r));
   
 });
 
-router.get('/ch/:chapterID', function(req, res, next) {
+router.get('/ch/:chapterID', (req, res, next) => {
   const chapterID = req.params.chapterID;
   const chapterURL = 'https://www.manhuaren.com/' + chapterID  
   getImgLinks(chapterURL).then((r) => res.send(r));
   
+});
+
+router.get('/sugg/1', (req, res, next) => {
+  // 
+
+  // getSuggestions('http://www.dm5.com/search.ashx?d=1530104913527&t=%E7%81%AB&language=1')
+  getSuggestions('http://www.dm5.com/search.ashx?d=1530114404442&t=%E4%B8%80&language=1')
+    .then((r) => res.send(r));
 });
 
 module.exports = router;
--- utils/manga-utils.js
+++ utils/manga-utils.js
@@ -69,13 +69,74 @@ const getChapterLinks = async (mangaURL) => {
     const aTags = $('#detail-list-select-1 a');
 
     for (let k in aTags) {
-      let attribs = aTags[k].attribs
-      if (attribs !== undefined && Object.keys(attribs).length !== 0) 
+      let aTag = aTags[k];
+      let attribs = aTag.attribs;
+      if (attribs !== undefined && Object.keys(attribs).length !== 0) {
+        attribs.vol = aTag.children[0].data;
         results.push(attribs);
+      }
     }
   })
   return results
+}
+
+const getText = (tags, flag) => {
+  let results = [];
+  let ends = [];
+  for (let i = 0; i < tags.length ; i++) {
+    let data = tags[i].children[0].data;
+    results.push(data === undefined ? '' : data);
+    if (flag) {
+      let length = tags[i].children.length;
+      let data2 = tags[i].children[length-1].data
+      ends.push(data2 === undefined ? '' : data2);
+    }
+  }
+  return [results, ends];
+}
+
+const allEqual = arr => arr.every( v => v === arr[0] )
+
+
+const getSuggestions = async (url) => {
+  // var nowEpoch = Date.now();
+  let results = [];
+  await getHTML(url)
+    .then(({data, url}) => {
+      const $ = data;
+      const liTags = $('li');
+
+      let links = []
+      for (let k in liTags) {
+        let liTag = liTags[k];
+        let attribs = liTag.attribs;
+        if (attribs !== undefined && Object.keys(attribs).length !== 0) {
+          links.push(attribs.onclick);
+        }        
+      }
+      const leftTags = $('li > a > span[class="left"]');
+      const rightTags = $('li > a > span[class="right"]');
+      const redTags = $('li > a > span > span[class="red"]');
+      
+      const reds = redTags.contents().map(function() {
+        if (this.type == 'text')
+          return $(this).text();
+      }).get();
+      const [lefts, ends] = getText(leftTags, true)
+      const [rights, ] = getText(rightTags, false)
+
+      for (let i = 0; i < lefts.length; i++) {
+
+        results.push({
+          title:lefts[i] + reds[i] + ends[i],
+          chapter: rights[i],
+          link: links[i]
+        });
+      }
+
+    })
+  return results;
 } 
 
 
-module.exports = {getChapterLinks, getImgLinks};
+module.exports = {getChapterLinks, getImgLinks, getSuggestions};
