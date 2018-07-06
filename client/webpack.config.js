2c44e738575b395c974489c8fbeaa3cfa879ed38
Harry@Seekers <harrychan@seekersadvisors.com>
2018-07-06 09:33:40 +0800

    add SERVER_URL

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,6 +26,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      SERVER_URL: JSON.stringify("http://localhost:5000/"),
     })
   ]
 };
